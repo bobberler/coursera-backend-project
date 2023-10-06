93aab29691edeb7c845c8f7d4ea2e26fb7ffc164
tom <tom@server.tomlums.com>
2023-10-06 11:48:53 -0400

    models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateField()
+
+    def __str__(self): 
+        return self.name
+
+
+# Add code to create Menu model
+class Menu(models.Model):
+   title = models.CharField(max_length=200) 
+   price = models.IntegerField(null=False) 
+   inventory = models.IntegerField(null=False)
+
+   def __str__(self):
+      return self.title
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin 
-from django.urls import path 
+from django.urls import path
+from . import views
 
-# urlpatterns = [ 
-#     path('', , ), 
-# ]
+urlpatterns = [ 
+    path('', views.index, name='index')
+]
